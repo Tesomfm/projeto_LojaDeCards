5603a505417c8ee3ed2314878a1d67a7aac74fd8
Matheus Henrique Fonseca Moreira <matheushfm0@gmail.com>
2026-06-30 19:11:30 -0300

    corrigindo bugs do servidor dnv

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,9 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="API projeto loja de cartas")
 
 origins =[
-    "https://projeto-loja-de-cards.vercel.app"
+    "https://projeto-loja-de-cards.vercel.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
 ]
 app.add_middleware(
     CORSMiddleware,
